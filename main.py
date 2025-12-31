f0465789a5de688fc6b3e810d71722d91307e63a
xnz233 <jl3362518@163.com>
2026-01-01 00:16:51 +0800

    refactor!: 重构下载菜单系统，提升可维护性
    - 实现 `DownloadOption` 类和 `@register_options` 装饰器
    
    BREAKING CHANGE:
    不再需要手动修改 `choice` 函数的选项列表，
    必须使用 `@register_options` 装饰器注册新的下载函数。

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import os
-import crawler
+from typing import Callable, List
+from dataclasses import dataclass
 from prompt_toolkit import prompt, choice
 from prompt_toolkit.shortcuts import confirm
 
+import crawler
 
+@dataclass
+class DownloadOption:
+    """管理下载方法的类
+    """
+    name: str
+    handler: Callable
+
+
+download_options: List[DownloadOption] = []
+
+
+def register_options(name:str):
+    """用于注册菜单的装饰器
+
+    Args:
+        name (str): 菜单名
+    """
+    def decorator(func):
+        option = DownloadOption(name, func)
+        download_options.append(option)
+        return func
+
+    return decorator
+
+
+@register_options("下载单首")
 def _download_single(need_lyric):
     song_name = prompt("请输入查找的歌曲名称:")
     song_list = crawler.search_music(song_name)
@@ -16,7 +44,9 @@ def _download_single(need_lyric):
     song.download(lyric=need_lyric)
 
 
-def _download_from_txt(need_lyric,filename: str='music.txt'):
+@register_options("批量下载")
+def _download_from_txt(need_lyric):
+    filename = "music.txt"
     if os.path.exists(filename):
         with open(filename) as f:
             for song_name in f:
@@ -26,18 +56,18 @@ def _download_from_txt(need_lyric,filename: str='music.txt'):
                 song.download(need_lyric)
     else:
         with open(filename, "w") as f:
-            print(f'已创建搜索文件{filename},填写后重新运行')
+            print(f"已创建搜索文件{filename},填写后重新运行")
             f.write(
                 "# 请在下面每输入要搜索的歌名，一行一首\n# 例子: always online 林俊杰"
             )
 
-print("MP3用户建议下载歌词")
-need_lyric = confirm("是否下载歌词? ", suffix="(Y/n)") or True
-download_music = choice(
-    "请选择下载方式",
-    options=[
-        (_download_single,'下载单首'),
-        (_download_from_txt,'批量下载歌曲')
-    ],
-)
-download_music(need_lyric)
+
+if __name__ == "__main__":
+    print("MP3用户建议下载歌词")
+    need_lyric = confirm("是否下载歌词? ", suffix="(Y/n)") or True
+
+    selected_option = choice(
+        "请选择下载方式",
+        options=[(option, option.name) for option in download_options],
+    )
+    selected_option.handler(need_lyric)
