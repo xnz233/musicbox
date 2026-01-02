5d0471592162b27a28803de096a9b6cea9a7d3c3
xnz233 <jl3362518@163.com>
2026-01-02 13:09:28 +0800

    fix: 修改交互逻辑避免闪退
    - 结尾增加提示避免直接退出
    - 创建文件后保持程序运行
    - 显式指定`music.txt`编码为`UTF-8`

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """一个交互式命令行界面，用于从网络上搜索和下载音乐。"""
+
 import os
 from typing import Callable, List
 from dataclasses import dataclass
@@ -7,24 +8,27 @@ from prompt_toolkit.shortcuts import confirm
 
 import crawler
 
+
 @dataclass
 class DownloadOption:
     """
     用于封装一个下载选项的元数据类。
     """
+
     name: str
     handler: Callable
 
 
-download_options: List[DownloadOption] = [] # 下载选项的全局列表
+download_options: List[DownloadOption] = []  # 下载选项的全局列表
 
 
-def register_options(name:str):
+def register_options(name: str):
     """用于注册菜单的装饰器工厂
 
     Args:
         name (str): 菜单名
     """
+
     def decorator(func):
         option = DownloadOption(name, func)
         download_options.append(option)
@@ -57,27 +61,24 @@ def _download_from_txt(need_lyric):
     并自动下载每首歌曲的第一个搜索结果。以 `#` 开头的行将被视为注释并忽略。
     """
     filename = "music.txt"
-    if os.path.exists(filename):
-        with open(filename) as f:
-            songs_name=[]
-            for song_name in f:
-                if song_name.startswith("#"): # 忽略#开头的行
-                    continue
-                songs_name.append(song_name)
-            songs:List[crawler.Song] = crawler.batch_search(songs_name)
-            crawler.batch_download(songs,need_lyric)
-    else:
-        with open(filename, "w") as f:
-            print(f"已创建搜索文件{filename},填写后重新运行")
+    if not os.path.exists(filename):
+        with open(filename, "w", encoding="UTF-8") as f:
+            print(f"已创建搜索文件 {filename} ,填写后保存")
             f.write(
-                "# 请在下面每输入要搜索的歌名，一行一首\n# 例子: always online 林俊杰"
+                "# 请在下面每输入要搜索的歌名，一行一首\n# 以'#'开头的行将被忽略\n"
             )
+        prompt('保存后请回车确认...')
+    with open(filename,encoding="UTF-8") as f:
+        songs_name = []
+        for song_name in f:
+            if song_name.startswith("#"):  # 忽略#开头的行
+                continue
+            songs_name.append(song_name)
+        songs: List[crawler.Song] = crawler.batch_search(songs_name)
+        crawler.batch_download(songs, need_lyric)
 
 
 if __name__ == "__main__":
-    """
-    程序的主执行入口。
-    """
     print("MP3用户建议下载歌词")
     need_lyric = confirm("是否下载歌词? ", suffix="(Y/n)") or True
 
@@ -86,3 +87,4 @@ if __name__ == "__main__":
         options=[(option, option.name) for option in download_options],
     )
     selected_option.handler(need_lyric)
+    prompt('按回车键退出...')
